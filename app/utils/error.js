b58eafb260f20f15b5afa6c36aab6e85e7056269
André Luiz da Silva <andreluiz.97@outlook.com>
2019-10-05 16:17:36 -0300

    Adiciona Error customizado

---
+++ app/utils/error.js
@@ -0,0 +1,14 @@
+/** Custom error */
+class Error {
+  
+  /**
+   * Creates an instance of Error.
+   * @param {string} name Name of the error
+   * @memberof Error
+   */
+  constructor (name) {
+    this.name = name
+  }
+}
+
+export default Error
