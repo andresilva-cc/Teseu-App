bb73c9fc07ca8db5c8671f831440dd6e4648a2ab
André Luiz da Silva <andreluiz.97@outlook.com>
2019-10-30 13:38:56 -0300

    Altera mensagem de confirmação de SMS

--- app/resources/lang/pt_BR/auth.js
+++ app/resources/lang/pt_BR/auth.js
@@ -8,7 +8,7 @@ export default {
   logoutConfirm: 'Você tem certeza que deseja sair? Você não receberá mais notificações',
   register: 'Registrar',
   sendSMSCode: 'Enviar código por SMS',
-  SMSCodeMessage: 'Um código foi enviado por SMS para seu número, por favor digite-o no campo abaixo',
+  SMSCodeMessage: 'Um código foi enviado por SMS para seu número, por favor digite-o no campo abaixo. Caso você não receba o código em até 3 minutos, uma ligação será feita para o seu número.',
   usernameMessage: 'Seu nome de usuário será visível para todos.',
   viewOnly: 'Somente visualizar',
   viewOnlyMessage: 'Se você não se autenticar, você não receberá notificações em tempo real.'
